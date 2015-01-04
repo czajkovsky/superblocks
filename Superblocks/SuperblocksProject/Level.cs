f0b0fd0e414d6680241ed1747bdb681c717f08c4
Mateusz Czajka <mateusz.czajka@netguru.pl>
2015-01-04 18:14:32 +0100

    init blocks creation

--- Superblocks/SuperblocksProject/Level.cs
+++ Superblocks/SuperblocksProject/Level.cs
@@ -9,6 +9,15 @@ namespace SuperblocksProject
     public Level(int id)
     {
       this.id = id;
+      CreateBlocks();
+    }
+    
+    private void CreateBlocks()
+    {
+      Console.WriteLine ("creating blocks...");
+      int[] blocksArray = { 1, 1, 1, 0, 1, 1, 0, 1, 1, 1,
+                            2, 2, 3, 3, 1, 1, 3, 3, 2, 2 };
+      
     }
   }
 }
