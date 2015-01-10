a0960181949301c7d4ac21a78ab2b67af6535197
Mateusz Czajka <mateusz.czajka@netguru.pl>
2015-01-10 16:41:29 +0100

    add block behaviour

---
+++ Superblocks/SuperblocksProject/BlockBehaviour.cs
@@ -0,0 +1,41 @@
+﻿#region Using Statements
+using System;
+using WaveEngine.Common.Input;
+using WaveEngine.Common.Math;
+using WaveEngine.Framework;
+using WaveEngine.Framework.Graphics;
+using WaveEngine.Framework.Services;
+using WaveEngine.Framework.Physics2D;
+#endregion
+
+namespace SuperblocksProject
+{
+  public class BlockBehaviour : Behavior
+  {
+    private enum BlockState { Pre, Running, Destroyed };
+    private BlockState currentState;
+    private Block block;
+    
+    public BlockBehaviour (Block block)
+    {
+      this.currentState = BlockState.Pre;
+      this.block = block;
+    }
+    
+    protected override void Update(TimeSpan gameTime)
+    {
+      RigidBody2D body = Owner.FindComponent<RigidBody2D>();
+
+      if (body != null && currentState == BlockState.Pre) {
+        body.OnPhysic2DCollision += onCollision;
+        currentState = BlockState.Running;
+      }
+    }
+
+    private void onCollision(object sender, Physic2DCollisionEventArgs args)
+    {
+      this.Owner.Scene.EntityManager.Remove (block.Name);
+    }
+  }
+}
+
