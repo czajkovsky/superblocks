e42832f325b45710dad655088085a566857921aa
Mateusz Czajka <mateusz.czajka@netguru.pl>
2015-01-10 16:41:37 +0100

    clean up pad

--- Superblocks/SuperblocksProject/Pad.cs
+++ Superblocks/SuperblocksProject/Pad.cs
@@ -18,18 +18,22 @@ namespace SuperblocksProject
 
     private int width, offset;
     private Entity entity;
-    
+
     public Pad ()
     {
       this.width = INITIAL_WIDTH;
       this.offset = WaveServices.Platform.ScreenWidth / 2;
-      
+
       this.entity = new Entity("pad")
-        .AddComponent(new Transform2D() { X = this.offset, Y = BOTTOM_OFFSET, Origin = Vector2.Center })
+        .AddComponent(new Transform2D() {
+          X = this.offset,
+          Y = BOTTOM_OFFSET,
+          Origin = Vector2.Center
+        })
         .AddComponent(new Sprite("textures/pad.wpk"))
-        .AddComponent(new RectangleCollider())
-        .AddComponent(new SpriteRenderer(DefaultLayers.Opaque))
-        .AddComponent(new RigidBody2D() { 
+        .AddComponent(new SpriteRenderer(DefaultLayers.Alpha))
+        .AddComponent(new PerPixelCollider("textures/pad.wpk", 1f))
+        .AddComponent(new RigidBody2D() {
           PhysicBodyType = PhysicBodyType.Dynamic,
           Mass = 400f,
           Damping = 1,
@@ -40,7 +44,7 @@ namespace SuperblocksProject
         })
         .AddComponent(new PadBehaviour(this));
     }
-    
+
     public Entity Entity { get { return entity; } private set { entity = value; } }
     public int Width { get { return width; } private set { width = value; } }
   }
