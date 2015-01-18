12966c549e29b5379580ad8c1120f08eed1f2a9e
Mateusz Czajka <mateusz.czajka@netguru.pl>
2015-01-18 21:35:54 +0100

    extract block body to another class

--- Superblocks/SuperblocksProject/Block.cs
+++ Superblocks/SuperblocksProject/Block.cs
@@ -14,71 +14,32 @@ namespace SuperblocksProject
 {
   public class Block
   {
-    private Entity entity;
     private int id, lives, type;
     private int[] blockInitialLives = { 0, 1, 2 };
-    private Dictionary<string, Sprite> sprites = new Dictionary<string, Sprite>();
-     
+    private BlockBody body;
+    
     public Block(int id, int offsetX, int offsetY, int type)
     {
       this.id = id;
       this.lives = blockInitialLives[type];
       this.type = type;
-      createSprites();
-            
-      this.entity = new Entity("block" + this.id)
-        .AddComponent(new Transform2D() {
-          X = blockXOffset(offsetX),
-          Y = blockYOffset(offsetY),
-          Origin = Vector2.Center
-        })
-        .AddComponent(sprites[("block_t" + this.type + "_l" + this.lives)])
-        .AddComponent(new RectangleCollider())
-        .AddComponent(new RigidBody2D() { PhysicBodyType = PhysicBodyType.Static })
-        .AddComponent(new SpriteRenderer(DefaultLayers.Alpha))
-        .AddComponent(new BlockBehaviour(this));
+      this.body = new BlockBody(offsetX, offsetY, this);
     }
       
-    private void createSprites() {
-      sprites.Add("block_t1_l1", new Sprite ("textures/block_t1_l1.wpk"));
-      sprites.Add("block_t2_l2", new Sprite ("textures/block_t2_l2.wpk"));
-      sprites.Add("block_t2_l1", new Sprite ("textures/block_t2_l1.wpk"));
-    }
-        
     public bool Hit() 
     {
       lives--;
       if (lives > 0) {
-        changeTexture();
+        body.ChangeTexture();
         return false;
       }
       return true;
     }
 
     public string Name { get { return "block" + id; } }
-    public Entity Entity { get { return entity; } private set { entity = value; } }
-    
-    private void changeTexture() 
-    {
-      this.entity
-        .RemoveComponent<Sprite>()
-        .AddComponent(sprites[("block_t" + type + "_l" + lives)])
-        .RefreshDependencies();
-    }
-    
-    private float blockXOffset(int offsetX) {
-      const int OFFSET_X = 20;
-      const int GUTTER_X = 4;
-      const int WIDTH = 116;
-      return OFFSET_X + GUTTER_X + offsetX * (WIDTH + 2 * GUTTER_X) + (int)((float)WIDTH / 2f);
-    }
-        
-    private float blockYOffset(int offsetY) {
-      const int HEIGHT = 51;
-      const int OFFSET_Y = 28;
-      const int GUTTER_Y = 4;
-      return OFFSET_Y + GUTTER_Y + offsetY * (HEIGHT + 2 * GUTTER_Y) + (int)((float)HEIGHT / 2f);
-    }   
+    public int Type { get { return type; } }
+    public int Lives { get { return lives; } }
+    public Entity Entity { get { return body.Entity; } }
   }
 }
 
---
+++ Superblocks/SuperblocksProject/BlockBody.cs
@@ -0,0 +1,71 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using WaveEngine.Common.Math;
+using WaveEngine.Components.Graphics2D;
+using WaveEngine.Framework;
+using WaveEngine.Framework.Graphics;
+using WaveEngine.Framework.Resources;
+using WaveEngine.Framework.Services;
+using WaveEngine.Framework.Physics2D;
+#endregion
+
+namespace SuperblocksProject
+{
+  public class BlockBody
+  {
+    private Entity entity;
+    private Dictionary<string, Sprite> sprites = new Dictionary<string, Sprite>();
+    private Block block;
+    
+    public BlockBody (int offsetX, int offsetY, Block block)
+    {
+      this.block = block;
+      this.sprites = createSprites();
+      this.entity = new Entity(block.Name)
+        .AddComponent(new Transform2D() {
+          X = blockXOffset(offsetX),
+          Y = blockYOffset(offsetY),
+          Origin = Vector2.Center
+        })
+        .AddComponent(sprites[("block_t" + block.Type + "_l" + block.Lives)])
+        .AddComponent(new RectangleCollider())
+        .AddComponent(new RigidBody2D() { PhysicBodyType = PhysicBodyType.Static })
+        .AddComponent(new SpriteRenderer(DefaultLayers.Alpha))
+        .AddComponent(new BlockBehaviour(block));
+    }
+    
+    private Dictionary<string, Sprite> createSprites() {
+      Dictionary<string, Sprite> sprites = new Dictionary<string, Sprite>();
+      sprites.Add("block_t1_l1", new Sprite ("textures/block_t1_l1.wpk"));
+      sprites.Add("block_t2_l2", new Sprite ("textures/block_t2_l2.wpk"));
+      sprites.Add("block_t2_l1", new Sprite ("textures/block_t2_l1.wpk"));
+      return sprites;
+    }
+    
+    public void ChangeTexture() 
+    {
+      this.entity
+        .RemoveComponent<Sprite>()
+        .AddComponent(sprites[("block_t" + block.Type + "_l" + block.Lives)])
+        .RefreshDependencies();
+    }
+
+    private float blockXOffset(int offsetX) {
+      const int OFFSET_X = 20;
+      const int GUTTER_X = 4;
+      const int WIDTH = 116;
+      return OFFSET_X + GUTTER_X + offsetX * (WIDTH + 2 * GUTTER_X) + (int)((float)WIDTH / 2f);
+    }
+
+    private float blockYOffset(int offsetY) {
+      const int HEIGHT = 51;
+      const int OFFSET_Y = 28;
+      const int GUTTER_Y = 4;
+      return OFFSET_Y + GUTTER_Y + offsetY * (HEIGHT + 2 * GUTTER_Y) + (int)((float)HEIGHT / 2f);
+    } 
+    
+    public Entity Entity { get { return entity; } private set { entity = value; } }
+  }
+}
+
