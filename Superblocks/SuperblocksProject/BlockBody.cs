f8561520b7a41b328467365f1d060c6bf6ae1587
Mateusz Czajka <mateusz.czajka@netguru.pl>
2015-01-18 21:39:20 +0100

    simplify sprite generation

--- Superblocks/SuperblocksProject/BlockBody.cs
+++ Superblocks/SuperblocksProject/BlockBody.cs
@@ -15,39 +15,29 @@ namespace SuperblocksProject
   public class BlockBody
   {
     private Entity entity;
-    private Dictionary<string, Sprite> sprites = new Dictionary<string, Sprite>();
     private Block block;
     
     public BlockBody (int offsetX, int offsetY, Block block)
     {
       this.block = block;
-      this.sprites = createSprites();
       this.entity = new Entity(block.Name)
         .AddComponent(new Transform2D() {
           X = blockXOffset(offsetX),
           Y = blockYOffset(offsetY),
           Origin = Vector2.Center
         })
-        .AddComponent(sprites[("block_t" + block.Type + "_l" + block.Lives)])
+        .AddComponent(new Sprite ("textures/block_t" + block.Type + "_l" + block.Lives + ".wpk"))
         .AddComponent(new RectangleCollider())
         .AddComponent(new RigidBody2D() { PhysicBodyType = PhysicBodyType.Static })
         .AddComponent(new SpriteRenderer(DefaultLayers.Alpha))
         .AddComponent(new BlockBehaviour(block));
     }
     
-    private Dictionary<string, Sprite> createSprites() {
-      Dictionary<string, Sprite> sprites = new Dictionary<string, Sprite>();
-      sprites.Add("block_t1_l1", new Sprite ("textures/block_t1_l1.wpk"));
-      sprites.Add("block_t2_l2", new Sprite ("textures/block_t2_l2.wpk"));
-      sprites.Add("block_t2_l1", new Sprite ("textures/block_t2_l1.wpk"));
-      return sprites;
-    }
-    
     public void ChangeTexture() 
     {
       this.entity
         .RemoveComponent<Sprite>()
-        .AddComponent(sprites[("block_t" + block.Type + "_l" + block.Lives)])
+        .AddComponent(new Sprite ("textures/block_t" + block.Type + "_l" + block.Lives + ".wpk"))
         .RefreshDependencies();
     }
 
@@ -65,7 +55,7 @@ namespace SuperblocksProject
       return OFFSET_Y + GUTTER_Y + offsetY * (HEIGHT + 2 * GUTTER_Y) + (int)((float)HEIGHT / 2f);
     } 
     
-    public Entity Entity { get { return entity; } private set { entity = value; } }
+    public Entity Entity { get { return entity; } }
   }
 }
 
