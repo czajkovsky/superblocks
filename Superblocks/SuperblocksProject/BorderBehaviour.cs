d2de75b315ede4cb49dd7347dd3ca1f7ae0e5e36
Mateusz Czajka <mateusz.czajka@netguru.pl>
2015-01-24 14:59:48 +0100

    fix low angles

--- Superblocks/SuperblocksProject/BorderBehaviour.cs
+++ Superblocks/SuperblocksProject/BorderBehaviour.cs
@@ -34,7 +34,41 @@ namespace SuperblocksProject
 
     private void onCollision(object sender, Physic2DCollisionEventArgs args)
     {
-      Console.WriteLine("collision...");
+      RigidBody2D ball = args.Body2DB;
+      Vector2 impulse = Vector2.Zero;
+      
+      if (Math.Abs (ball.LinearVelocity.Y) < 0.3f) {
+        float factor = 0.07f;
+        float impulseY = factor * ((ball.LinearVelocity.Y > 0) ? 1f : -1f);
+        float impulseX = changeX (ball.LinearVelocity.X, ball.LinearVelocity.Y, factor);
+        impulse += Vector2.UnitX * impulseX;
+        impulse += Vector2.UnitY * impulseY;
+    
+        float x = ball.LinearVelocity.X, y = ball.LinearVelocity.Y;
+        float f1 = x * x + y * y;
+        x += impulse.X;
+        y += impulse.Y;
+        float f2 = x * x + y * y;
+        float episolon = 0.05f;
+        if (Math.Abs (f1 - f2) < episolon)
+          ball.ApplyLinearImpulse (impulse);
+      }
+    }
+    
+    private void quadraticEquation(float a, float b, float c, out float result1, out float result2)
+    {
+      float delta = b * b - 4 * a * c;
+      result1 = (-1f * b - (float)Math.Sqrt((double)delta)) / 2 / a;
+      result2 = (-1f * b + (float)Math.Sqrt((double)delta)) / 2 / a;
+    }
+    
+    private float changeX(float x, float y, float factor)
+    {
+      float c = (float)Math.Pow ((double)(y + factor), 2) - y * y;
+      float result1, result2;
+      quadraticEquation(1f, 2f * x, c, out result1, out result2);
+      Console.WriteLine ("x1: " + result1 + ", x2: " + result2);
+      return (Math.Abs(result1) > Math.Abs(result2) ? result2 : result1);
     }
   }
 }
