523adc94157adb1d9adddbb628155a1907a78b88
Mateusz Czajka <mateusz.czajka@netguru.pl>
2015-01-31 13:48:06 +0100

    adding bonus

--- Superblocks/SuperblocksProject/Source/Managers/BonusesManager.cs
+++ Superblocks/SuperblocksProject/Source/Managers/BonusesManager.cs
@@ -10,16 +10,21 @@ namespace SuperblocksProject
   public class BonusesManager
   {
     private Level level;
+    private int sequence;
     
     public BonusesManager (Level level)
     {
       this.level = level;
+      this.sequence = 0;
     }
     
     public void CreateBonus(float x, float y)
     {
-      Console.WriteLine (x + " " + y);
-      
+      System.Random random = new System.Random ();
+      if (random.Next(0, 10) > -1) {
+        Bonus bonus = new Bonus(this.sequence++);
+        level.Game.LevelScene.EntityManager.Add(bonus.Entity);
+      }
     }
   }
 }
